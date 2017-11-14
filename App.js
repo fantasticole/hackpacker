3d78dfcc546f66cbaa7e6e88e129a9164e6fe46b
Cole Murphy <cole.murphy@venuebook.com>
2017-11-14 18:00:55 -0500

    Add full screen background image

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Button,
+  Image,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -14,17 +15,36 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HackPacker</Text>
-        <Text>What are you here to do?</Text>
-        <Button
-          onPress={this.onPressChangeMoney}
-          style={styles.button}
-          title="Change Money"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <TouchableOpacity onPress={this.onPressChangeMoney}>
-          <Text style={styles.button}>Change Money</Text>
-        </TouchableOpacity>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Image source={require('./images/pattern1.png')} style={styles.backgroundImage} />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            justifyContent: 'center',
+          }}
+        >
+          <Text style={styles.text}>HackPacker</Text>
+          <Text style={styles.text}>What are you here to do?</Text>
+          <Button
+            onPress={this.onPressChangeMoney}
+            style={styles.button}
+            title="Change Money"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <TouchableOpacity onPress={this.onPressChangeMoney}>
+            <Text style={styles.button}>Change Money</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -36,6 +56,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+    marginTop: 20,
   },
   button: {
     backgroundColor: '#000',
@@ -45,4 +68,17 @@ const styles = StyleSheet.create({
     paddingLeft: 30,
     paddingRight: 30,
   },
+  text: {
+    padding: 15,
+    backgroundColor: '#fff',
+    // paddingTop: 15,
+    // paddingBottom: 15,
+    // paddingLeft: 30,
+    // paddingRight: 30,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'repeat',
+    // resizeMode: 'cover', // or 'stretch'
+  }
 });
